d1e6df21dfcc7b19c74dff1afbace8789d342a9f
florent <flopastor06@gmail.com>
2026-01-12 17:09:49 +0100

    fix unecessary calls, improve image resolution

--- custom_components/windy_webcams/api.py
+++ custom_components/windy_webcams/api.py
@@ -1,6 +1,7 @@
 """API for Windy."""
 from datetime import datetime
 from io import BytesIO
+from urllib.error import HTTPError
 
 from requests import get
 
@@ -19,14 +20,26 @@ def get_webcam_json_data(webcam_id, api_key):
     return data
 
 
-def get_image_url(json_data):
+def extract_image_preview_url(json_data):
+    if 'images' not in json_data:
+        return None
     return json_data["images"]["current"]["preview"]
 
-def get_image_last_updated(json_data):
-    str_value = json_data["lastUpdatedOn"]
-    return datetime.fromisoformat(str_value)
+def extract_image_full_url(json_data):
+    preview = extract_image_preview_url(json_data)
+    if preview is None:
+        return None
+    return preview.replace('preview', 'full')
+
+def extract_image_last_updated(json_data):
+    if 'lastUpdatedOn' in json_data:
+        str_value = json_data["lastUpdatedOn"]
+        return datetime.fromisoformat(str_value)
+    return None
 
 
 def get_image_bytes(url):
     response = get(url)
+    if response.status_code == 404:
+        return None
     return BytesIO(response.content).getvalue()
--- custom_components/windy_webcams/image.py
+++ custom_components/windy_webcams/image.py
@@ -1,3 +1,4 @@
+import logging
 from typing import LiteralString
 
 from homeassistant.components.image import ImageEntity
@@ -7,41 +8,49 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .api import (
+    extract_image_full_url,
+    extract_image_last_updated,
+    extract_image_preview_url,
     get_image_bytes,
-    get_image_last_updated,
-    get_image_url,
     get_webcam_json_data,
 )
 from .const import CONF_IDS
 
+_LOGGER = logging.getLogger(__name__)
 
 class WebcamImageEntity(ImageEntity):
     def __init__(
         self, hass: HomeAssistant, api: LiteralString, webcam_id: LiteralString
     ) -> None:
-        """Initialize the Workday sensor."""
+        """Initialize the Webcam image sensor."""
         super().__init__(hass)
         self._attr_unique_id = webcam_id
         self.webcam_id = webcam_id
         self.api = api
 
-    def _get_image(self) -> bytes:
-        json = get_webcam_json_data(self.webcam_id, self.api)
-        url = get_image_url(json)
-        data = get_image_bytes(url)
-        return data
+    def _get_webcam_data_json(self):
+        return get_webcam_json_data(self.webcam_id, self.api)
 
-    def _get_updated(self) -> bytes:
-        json = get_webcam_json_data(self.webcam_id, self.api)
-        return get_image_last_updated(json)
+    def _get_image(self, json) -> bytes | None:
+        url = extract_image_preview_url(json)
+        full_url = extract_image_full_url(json)
+        if url is None and full_url is None:
+            return None
+        full_image = get_image_bytes(full_url)
+        if full_image is not None:
+            return full_image
+        preview_image = get_image_bytes(url)
+        if preview_image is not None:
+            return preview_image
+        _LOGGER.warning('Image could not be downloaded %s', self.webcam_id)
+        return None
 
     async def async_image(self) -> bytes | None:
         """Return bytes of image."""
-        data = await self.hass.async_add_executor_job(self._get_image)
-        updated = await self.hass.async_add_executor_job(self._get_updated)
-        self.image_last_updated = updated
+        json = await self.hass.async_add_executor_job(self._get_webcam_data_json)
+        data = await self.hass.async_add_executor_job(self._get_image, json)
+        self.image_last_updated = extract_image_last_updated(json)
         self.async_write_ha_state()
-        print("update")
         return data
 
 
@@ -51,8 +60,9 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Windy Platform from config_flow."""
-    api = entry.as_dict()["options"][CONF_API_KEY]
+    options = entry.as_dict()["options"]
+    api = options[CONF_API_KEY]
     ids = [
-        w_id.strip() for w_id in entry.as_dict()["options"][CONF_IDS].split(",")
+        w_id.strip() for w_id in options[CONF_IDS].split(",")
     ]
     async_add_entities([WebcamImageEntity(hass, api, w_id) for w_id in ids])
